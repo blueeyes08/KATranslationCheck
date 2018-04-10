cfe8aa171c7ecc820485419bbe5b77d1eee709fa
Uli Köhler <ulikoehler@online.de>
2018-04-10 23:57:07 +0200

    Update runner for new server infrastructure

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 git pull
 # Download
-./katc.py -l de update-translations -j 32
-#./katc.py -l pt-BR update-translations -j 32
-#./katc.py -l bg update-translations -j 32
-./katc.py -l sv-SE update-translations -j 32
-./katc.py -l ja update-translations -j 32
-./katc.py -l ka update-translations -j 32
-./katc.py -l hu update-translations -j 32
-./katc.py -l cs update-translations -j 32
+./katc.py -l de update-translations -j 256
+./katc.py -l pt-BR update-translations -j 256
+/katc.py -l bg update-translations -j 256
+./katc.py -l sv-SE update-translations -j 256
+./katc.py -l ja update-translations -j 256
+./katc.py -l ka update-translations -j 256
+./katc.py -l hu update-translations -j 256
+./katc.py -l cs update-translations -j 256
 
 # Render
-./katc.py -l de render -f 2_hig
+./katc.py -l de render -f 2_high
 #./katc.py -l pt-BR render -f 2_high
 ./katc.py -l bg render -f 2_high
 ./katc.py -l hu render -f 2_high

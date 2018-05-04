92664fd24684e9acd162e4ff1e1830201234ddea
Uli Köhler <ulikoehler@online.de>
2018-05-04 05:08:31 +0200

    New concept for cloud datastore benchmark

--- BenchmarkGoogleCloudDatastore.py
+++ BenchmarkGoogleCloudDatastore.py
@@ -5,23 +5,10 @@ import time
 client = datastore.Client(project="watts-198422")
 
 # Create & store an entity
-query = client.query(kind='MyKind')
-
-StringRef = namedtuple("StringRef", [])
-
-
-def add_string_to_pattern(pattern, sref):
-    key = client.key('Pattern', pattern)
+def add_string_to_pattern(sref):
+    key = client.key('String', sref["id"])
     entity = client.get(key)
-    # Update entity
-    if "strings" not in entity:
-        entity.update({"strings": []})
-    # Is string already present
-    for existingSref in entity["strings"]:
-        if existingSref["id"] == sref["id"]:
-            return # No need to update
-    # Add string
-    entity["strings"].append(sref)
+    entity.update(sref)
     # Save
     client.put(entity)
 
@@ -29,23 +16,16 @@ s1 = {
     "id": 123,
     "source": "$1$",
     "target": "$1$",
+    "pattern": "$formula$",
     "target_source": "universal",
-    "state": "untranslated",
-    "file": "learn.drafts.xliff"
-}
-
-s2 = {
-    "id": 345,
-    "source": "$1$",
-    "target": "$1$",
-    "target_source": "universal",
-    "state": "untranslated",
+    "translated": False,
+    "approved": False,
     "file": "learn.drafts.xliff"
 }
 
 t0 = time.time()
 for i in range(1000):
     s1["id"] = i;
-    add_string_to_pattern("$formula$", s1)
+    write_string(s1)
 t1 = time.time()
 print((t1 - t0))

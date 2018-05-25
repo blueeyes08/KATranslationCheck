7bfb369e04e3940e6472832961d2de6ba8f46165
Uli Köhler <ulikoehler@online.de>
2018-05-25 19:18:12 +0000

    Added more langs to backup

--- backup-datastore.sh
+++ backup-datastore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Backupping DE"
+echo "Backupping de"
 gcloud beta datastore export --namespaces="de" gs://katc-datastorage-backup-1
 echo "Backupping sv-SE"
 gcloud beta datastore export --namespaces="sv-SE" gs://katc-datastorage-backup-1
@@ -9,3 +9,10 @@ echo "Backupping pl"
 gcloud beta datastore export --namespaces="pl" gs://katc-datastorage-backup-1
 echo "Backupping cs"
 gcloud beta datastore export --namespaces="cs" gs://katc-datastorage-backup-1
+echo "Backupping sr"
+gcloud beta datastore export --namespaces="sr" gs://katc-datastorage-backup-1
+echo "Backupping da"
+gcloud beta datastore export --namespaces="da" gs://katc-datastorage-backup-1
+echo "Backupping pt-PT"
+gcloud beta datastore export --namespaces="pt-PT" gs://katc-datastorage-backup-1
+

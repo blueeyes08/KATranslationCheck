e7ff1a8b576c47ea9147fe47f39400eb4b7a098b
Uli Köhler <ulikoehler@online.de>
2018-05-28 19:26:59 +0000

    Also update texttags on lang update

--- update-lang-datastore.sh
+++ update-lang-datastore.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./katc.py -l $1 update-translations -j128 && ./DatastoreImportStrings.py $1 && ./DatastoreIndexPatterns.py $1
+./katc.py -l $1 update-translations -j128 && ./DatastoreImportStrings.py $1 && ./DatastoreIndexPatterns.py $1 && ./DatastoreImportTexttags.py $1

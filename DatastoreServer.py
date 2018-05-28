8f4c3cad10fc3ed07996e05b4e0e28cee758eec4
Uli Köhler <ulikoehler@online.de>
2018-05-29 01:15:10 +0200

    Fix

--- DatastoreServer.py
+++ DatastoreServer.py
@@ -425,11 +425,11 @@ def index(lang):
     strings = [doc for doc in query_iter]
 
     for string in strings:
-        entry["id"] = entry.key.id_or_name
+        string["id"] = string.key.id_or_name
         # Remove unneccessary fields
-        for key in list(entry.keys()):
+        for key in list(string.keys()):
             if key.startswith("has_") or key in ["words", "source_length", "relevant_for_live"]:
-                del entry[key]
+                del string[key]
         # Add "original translation field"
         string.update({
             "original_translation": string["target"]

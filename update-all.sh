222841eb75131485a8b4ea25c910e12e53fc7978
Uli Köhler <ulikoehler@online.de>
2018-05-30 13:36:31 +0000

    Added update all script

---
+++ update-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+./update-lang-datastore.sh de
+./update-lang-datastore.sh sv-SE
+./update-lang-datastore.sh ru
+./update-lang-datastore.sh pl
+./update-lang-datastore.sh cs
+./update-lang-datastore.sh sr
+./update-lang-datastore.sh da
+./update-lang-datastore.sh pt-PT
+./update-lang-datastore.sh nb
+./update-lang-datastore.sh ka

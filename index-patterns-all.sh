0bd00e9da8aa75250b0985266029dcc5ba265859
Uli Köhler <ulikoehler@online.de>
2018-06-12 16:07:01 +0200

    Added all pattern idxing script

---
+++ index-patterns-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+./DatastoreIndexPatterns.py de
+./DatastoreIndexPatterns.py sv-SE
+./DatastoreIndexPatterns.py ru
+./DatastoreIndexPatterns.py pl
+./DatastoreIndexPatterns.py cs
+./DatastoreIndexPatterns.py sr
+./DatastoreIndexPatterns.py da
+./DatastoreIndexPatterns.py pt-PT
+./DatastoreIndexPatterns.py nb
+./DatastoreIndexPatterns.py ka
